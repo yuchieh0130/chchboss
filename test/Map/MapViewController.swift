f622eb78b601d6b5adb7a27b143236cba1d4ab30
Stasia <45786914+HsinJouHung@users.noreply.github.com>
2020-04-08 19:48:38 +0800

    Revert "location category"
    This reverts commit 1ac840f5fab0e84d26656a07514db660a75e5b4f.

--- test/Map/MapViewController.swift
+++ test/Map/MapViewController.swift
@@ -132,7 +132,6 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
             request.naturalLanguageQuery = searchQuery
             
             let search = MKLocalSearch(request: request)
-            var locationList = [String]()
             
             // 搜尋附近地點的結果
             search.start { (response, error) in
@@ -151,10 +150,8 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
                     let locationName: String? = self.searchResults[i].name
                     self.location_arr.append(locationName!)
                     let distance = CLLocation(latitude: (self.searchResults[i].placemark.coordinate.latitude), longitude: (self.searchResults[i].placemark.coordinate.longitude)).distance(from: CLLocation(latitude: (25.035915), longitude: (121.563619)))
-                    locationList.append(locationName)
-                    locationList.append(searchQuery)
                     if distance <= 1000{
-                        self.locationDic[distance] = locationList
+                        self.locationDic[distance] = locationName
                     }
                     i += 1
                     
@@ -218,7 +215,7 @@ extension MapViewController: UITableViewDataSource, UITableViewDelegate{
         }
         sortedArr.removeAll()
         for each in sortedLocation{
-            sortedArr.append(each.value[1]+each.value[0]+"  "+String(Int(each.key))+"m")
+            sortedArr.append(each.value+"  "+String(Int(each.key))+"m")
         }
         sortedLocation.removeAll()
         if searching{
