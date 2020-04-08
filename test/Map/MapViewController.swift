1807031c78c2488c27dcaa2d7ce49338a6d4dc2b
vincent88322 <40335326+vincent88322@users.noreply.github.com>
2020-04-08 19:25:32 +0800

    Revert "location category"
    This reverts commit 3a5dd5382ed229f66d3005202a51eb7b72358f16.

--- test/Map/MapViewController.swift
+++ test/Map/MapViewController.swift
@@ -78,7 +78,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     var location_arr = [String]()
     var sortedArr = [String]()
     var locationDic = [Double: String]()
-    
+   
     func locationManager(_ manager: CLLocationManager,
                          didUpdateLocations locations: [CLLocation]){
         
@@ -132,7 +132,6 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
             request.naturalLanguageQuery = searchQuery
             
             let search = MKLocalSearch(request: request)
-            var locationList = [String]()
             
             // 搜尋附近地點的結果
             search.start { (response, error) in
@@ -151,17 +150,14 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
                     let locationName: String? = self.searchResults[i].name
                     self.location_arr.append(locationName!)
                     let distance = CLLocation(latitude: (self.searchResults[i].placemark.coordinate.latitude), longitude: (self.searchResults[i].placemark.coordinate.longitude)).distance(from: CLLocation(latitude: (25.035915), longitude: (121.563619)))
-                    locationList.append(locationName)
-                    locationList.append(searchQuery)
                     if distance <= 1000{
-                        self.locationDic[distance] = locationList
+                        self.locationDic[distance] = locationName
                     }
                     i += 1
                     
                 }
                 self.searchAllResults.removeAll()
                 self.searchResults.removeAll()
-                locationList.removeAll()
                 self.tblView.reloadData()
                 
             }
@@ -219,7 +215,7 @@ extension MapViewController: UITableViewDataSource, UITableViewDelegate{
         }
         sortedArr.removeAll()
         for each in sortedLocation{
-            sortedArr.append(each.value[1]+ each.value[0]+"  "+String(Int(each.key))+"m")
+            sortedArr.append(each.value+"  "+String(Int(each.key))+"m")
         }
         sortedLocation.removeAll()
         if searching{
