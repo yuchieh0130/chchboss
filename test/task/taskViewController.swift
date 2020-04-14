20424f207cdffda14a386b829fe12bec1ecb80d1
張濰絜 <andrey870609@gmail.com>
2020-04-14 12:29:18 +0800

    task update

--- test/task/taskViewController.swift
+++ test/task/taskViewController.swift
@@ -18,15 +18,28 @@ class taskViewController: UIViewController, UITableViewDelegate, UITableViewData
     var taskId :Int32?
     var task: TaskModel?
     
+    var selectedTask: String = ""
+    var showTask = [TaskModel]()
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let editVC = segue.destination as? addTaskViewController{
-            editVC.task = task
+        switch segue.identifier {
+        case "addTask":
+            if let addVC: addTaskViewController = segue.destination as? addTaskViewController {
+                addVC.task = task
+            }
+        case "editTask":
+            if let editVC: addTaskViewController = segue.destination as? addTaskViewController{
+                editVC.task = task
+            }
+        default:
+            print("")
         }
+        
     }
     
-    @IBAction func EventSegueBack(segue: UIStoryboardSegue){
-        tableView.reloadData()
-    }
+//    @IBAction func EventSegueBack(segue: UIStoryboardSegue){
+//        tableView.reloadData()
+//    }
 
     @IBAction func addTask(_ sender: Any) {
         task = nil
@@ -34,28 +47,32 @@ class taskViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         title = "Task"
+    
+        tableView.delegate = self
+        tableView.dataSource = self
         
         //註冊.xib檔
         self.tableView.register(UINib(nibName: "taskTableViewCell", bundle: nil), forCellReuseIdentifier: "taskTableViewCell")
     }
     
     override func viewWillAppear(_ animated: Bool) {
-         super.viewWillAppear(animated)
+         //super.viewWillAppear(animated)
+        tableView.reloadData()
      }
      
      override func viewDidAppear(_ animated: Bool) {
          super.viewDidAppear(animated)
      }
     
-    var showTask = [TaskModel]()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             return showTask.count
         }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "taskTableViewCell", for: indexPath) as! taskTableViewCell
+        let cell:taskTableViewCell = tableView.dequeueReusableCell(withIdentifier: "taskTableViewCell", for: indexPath) as! taskTableViewCell
         cell.taskName?.text = showTask[indexPath.row].taskName
         cell.addTaskTime.text = showTask[indexPath.row].addTaskTime
         
