4346a2730a6266833387c7990cf9a32eb00366cd
張濰絜 <andrey870609@gmail.com>
2020-04-29 10:01:39 +0800

    add action VC move tabBarVC

--- test/tabBarController.swift
+++ test/tabBarController.swift
@@ -9,12 +9,12 @@
 import Foundation
 import UIKit
 
-class tabBarController: UITabBarController{
+class tabBarController: UITabBarController, UITabBarControllerDelegate{
     override func viewDidLoad() {
         tabBar.items?[0].title = "Calendar"
         tabBar.items?[1].title = "Track"
-        tabBar.items?[2].title = "Task"
-        tabBar.items?[3].title = "Analysis"
+        tabBar.items?[3].title = "Task"
+        tabBar.items?[4].title = "Analysis"
     }
     
 }
