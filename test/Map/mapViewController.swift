9bfd7c6a9c7e184043520dd7ba4e63096446dc2f
Stasia <45786914+HsinJouHung@users.noreply.github.com>
2020-05-13 10:32:59 +0800

    touchesbegan

--- test/Map/mapViewController.swift
+++ test/Map/mapViewController.swift
@@ -222,11 +222,20 @@ class mapViewController: UIViewController, UITableViewDataSource,CLLocationManag
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+
+}
+
+extension mapViewController: UISearchBarDelegate, UITextFieldDelegate{
+    
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+          textField.resignFirstResponder()
+          return true
+    }
 }
 
 
