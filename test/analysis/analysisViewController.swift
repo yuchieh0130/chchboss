b9fdbb0a24688b22b424a668473c9c7c3cd297cf
宛軒 <s22596463@users.noreply.github.com>
2020-06-10 17:56:08 +0800

    Update analysisViewController.swift

--- test/analysis/analysisViewController.swift
+++ test/analysis/analysisViewController.swift
@@ -26,7 +26,7 @@ class analysisViewController: UIViewController {
     let goals = [6, 8, 26, 30, 8, 10]
     let sports = ["Tennis", "Basketball", "Baseball", "Golf"]
     let counts = [45, 76, 34, 97]
-    let yep = [34, 67, 89, 45, 44, 12, 28, 90, 5]
+    let yep = [34, 67, 89, 45, 44, 12, 28, 90, 23]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,14 +52,21 @@ class analysisViewController: UIViewController {
         if getIndex == 0{
             customizePieChart(dataPoints: players, values: goals.map{ Double($0) })
             barChart.isHidden = true
+            pieChart.drawEntryLabelsEnabled = true
+            pieChart.entryLabelColor = UIColor.white
         }else if getIndex == 1{
             customizePieChart(dataPoints: sports, values: counts.map{
                 Double($0) })
+            pieChart.drawEntryLabelsEnabled = true
+            pieChart.entryLabelColor = UIColor.white
             barChart.isHidden = true
         }else if getIndex == 2{
-            customizePieChart(dataPoints: showCategoryStr, values: yep.map{ Double($0)})
+            customizeCategoryChart(dataPoints: showCategoryStr, values: yep.map{ Double($0)})
+            pieChart.entryLabelColor = UIColor.black
+            pieChart.drawEntryLabelsEnabled = false
             barChart.isHidden = true
         }
+        
     }
     
     func customizePieChart(dataPoints: [String], values: [Double]) {
@@ -98,6 +105,7 @@ class analysisViewController: UIViewController {
       format.numberStyle = .none
       let formatter = DefaultValueFormatter(formatter: format)
       pieChartData.setValueFormatter(formatter)
+        pieChartData.setValueTextColor(UIColor.black)
       // 4. Assign it to the chart’s data
       pieChart.data = pieChartData
     }
@@ -133,8 +141,9 @@ class analysisViewController: UIViewController {
     
     private func colorsOfCategory(numbersOfColor: Int) -> [UIColor] {
         var colors: [UIColor] = []
-        for _ in 0..<numbersOfColor{
-            let color = showCategoryColor
+        for i in 0...numbersOfColor-1{
+            let color = hexStringToUIColor (hex: "\(showCategoryColor[i])")
+            colors.append(color)
         }
         return colors
     }
@@ -149,7 +158,27 @@ class analysisViewController: UIViewController {
 //    ChartColorTemplates.vordiplom()
 //    ChartColorTemplates.material()
     
-   
+   func hexStringToUIColor (hex:String) -> UIColor {
+       var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+       if (cString.hasPrefix("#")) {
+           cString.remove(at: cString.startIndex)
+       }
+
+       if ((cString.count) != 6) {
+           return UIColor.gray
+       }
+
+       var rgbValue:UInt64 = 0
+       Scanner(string: cString).scanHexInt64(&rgbValue)
+
+       return UIColor(
+           red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+           green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+           blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+           alpha: CGFloat(0.8)
+       )
+   }
     
     
 }
