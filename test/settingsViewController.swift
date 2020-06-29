2c70b2f9a4a8cc06d3825f83601ae93d553c4c06
張濰絜 <andrey870609@gmail.com>
2020-06-29 23:37:23 +0800

    update

--- test/settingsViewController.swift
+++ test/settingsViewController.swift
@@ -14,6 +14,7 @@ class settingsViewController: UIViewController, UITableViewDelegate, UITableView
     @IBOutlet var tableView: UITableView!
     @IBOutlet var userIcon: UIImageView!
     @IBOutlet var userName: UILabel!
+    @IBOutlet var userID: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
