985e0355eac3e53eee913fca21568b15c9bc9b92
HsinJouHung <45786914+HsinJouHung@users.noreply.github.com>
2020-07-05 17:27:22 +0800

    login and signup end editing

--- test/Login/LoginViewController.swift
+++ test/Login/LoginViewController.swift
@@ -11,4 +11,33 @@ import UIKit
 
 class LoginViewController: UIViewController{
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
 }
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       self.view.endEditing(true)
+       return true
+    }
+}
+
