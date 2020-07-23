cdce343cb77c27a3fd2eba32d1f003e70f44e5b7
vincent88322 <40335326+vincent88322@users.noreply.github.com>
2020-07-23 18:20:45 +0800

    network

--- test/NetworkController.swift
+++ test/NetworkController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class NetworkController {
 // API URL
-    let baseURL = URL(string: "http://140.119.19.18:5000/")!
+    let baseURL = URL(string: "http://140.119.19.42:5000/")!
     func postLocationData (data: [String: String], completion: @escaping(Int?) -> Void) {
         let locationURL = baseURL.appendingPathComponent("insertLocation")
         var request = URLRequest(url: locationURL)
@@ -34,6 +34,7 @@ class NetworkController {
         }
         task.resume( )
     }
+    
     func postSaveplaceData (data: [String: String], completion: @escaping(Int?) -> Void) {
             let saveplaceURL = baseURL.appendingPathComponent("insertSaveplace")
             var request = URLRequest(url: saveplaceURL)
@@ -58,8 +59,8 @@ class NetworkController {
             task.resume( )
         }
 
-    func postTrackData (data: [String: String], completion: @escaping(Int?) -> Void) {
-            let trackURL = baseURL.appendingPathComponent("insertTrack")
+    func updateTrackData (data: [String: String], completion: @escaping(Int?) -> Void) {
+            let trackURL = baseURL.appendingPathComponent("updateTrack")
             var request = URLRequest(url: trackURL)
             request.httpMethod = "POST"
             request.setValue("application/json", forHTTPHeaderField:
@@ -81,30 +82,31 @@ class NetworkController {
             }
             task.resume( )
         }
-//    func postCategoryData (data: [String: String], completion: @escaping(Int?) -> Void) {
-//            let categoryURL = baseURL.appendingPathComponent("insertCategory")
-//            var request = URLRequest(url: categoryURL)
-//            request.httpMethod = "POST"
-//            request.setValue("application/json", forHTTPHeaderField:
-//               "Content-Type")
-//            let jsonEncoder = JSONEncoder()
-//            let jsonData = try? jsonEncoder.encode(data)
-//            request.httpBody = jsonData
-//            let task = URLSession.shared.dataTask(with: request)
-//            { (data, response, error) in
-//                    if let data = data,
-//                        let jsonDictionary = try?
-//                        JSONSerialization.jsonObject(with: data) as?
-//                        [String: Int],
-//                        let status_code = jsonDictionary["status_code"] {
-//                                completion(status_code)
-//                        } else {
-//                            completion(nil)
-//                        }
-//            }
-//            task.resume( )
-//        }
-//}
+    func register (user_id: String, password: String, user_name: String, completion: @escaping([Any]?) -> Void) {
+        let registerURL = baseURL.appendingPathComponent("register")
+        var request = URLRequest(url: registerURL)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField:
+           "Content-Type")
+        let data: [String: String] = ["user_id": user_id, "password": password, "user_name": user_name]
+        let jsonEncoder = JSONEncoder( )
+        let jsonData = try? jsonEncoder.encode(data)
+        request.httpBody = jsonData
+        let task = URLSession.shared.dataTask(with: request)
+            { (data, response, error) in
+                    if let data = data,
+                        let jsonDictionary = try?
+                        JSONSerialization.jsonObject(with: data) as?
+                        [String: Any],
+                        let status_code = jsonDictionary["status_code"] as? Int,
+                            let session_id = jsonDictionary["session_id"] as? String{
+                                completion([status_code, session_id])
+                        } else {
+                            completion(nil)
+                        }
+            }
+        task.resume( )
+    }
 
 
 }
