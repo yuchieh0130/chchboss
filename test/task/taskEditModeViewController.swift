51482bbb14adf848b708e4d3bad08f411377a2f4
張濰絜 <andrey870609@gmail.com>
2020-08-21 11:58:43 +0800

    task edit mode

---
+++ test/task/taskEditModeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  taskEditModeViewController.swift
+//  test
+//
+//  Created by 王義甫 on 2020/8/21.
+//  Copyright © 2020 AppleInc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class taskEditModeViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
