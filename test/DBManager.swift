3bd75f5005f7c7e792181c9d6489d9724ef2ab78
張濰絜 <106306087@mail2.nccu.tw>
2020-10-08 10:39:59 +0800

    Update DBManager.swift

--- test/DBManager.swift
+++ test/DBManager.swift
@@ -797,6 +797,40 @@ class DBManager: NSObject {
         return tracks
     }
     
+    func getCategoryTracks(Category: Int) -> [TrackModel]!{
+        let select = "\(Category)"
+        var tracks: [TrackModel]!
+        shareInstance.database?.open()
+        let sqlString =  "select * from track where strftime('%Y-%m',start_date)='\(select)' or strftime('&Y-%m',end_date)='\(select)'"
+        //           let sqlString = "SELECT * FROM track WHERE (start_date || ' ' || start_time) BETWEEN '\(String+" 00:00" )' and '\(String+" 23:59" )' or (end_date || ' ' || end_time) BETWEEN '\(String+" 00:00" )' and '\(String+" 23:59" )' "
+        
+        //let sqlString = "SELECT * FROM track WHERE start_date <= '\(String)' and end_date >= '\(String)' ORDER BY start_date ASC,start_time ASC";
+        let set = try?shareInstance.database?.executeQuery(sqlString, values: [])
+        
+        while ((set?.next())!) {
+            let i = set?.int(forColumn: "track_id")
+            let a = set?.string(forColumn: "start_date")
+            let b = set?.string(forColumn: "start_time")
+            let w = set?.int(forColumn: "weekDay")
+            let c = set?.string(forColumn: "end_date")
+            let d = set?.string(forColumn: "end_time")
+            let e = set?.int(forColumn: "category_id")
+            let f = set?.int(forColumn: "location_id")
+            let g = set?.int(forColumn: "place_id")
+            
+            let track: TrackModel
+            
+            if tracks == nil{
+                tracks = [TrackModel]()
+            }
+            
+            track = TrackModel(trackId: i!, startDate: a!, startTime: b!, weekDay: w! ,endDate: c!, endTime: d!,categoryId: e!, locationId: f!, placeId: g!)
+            tracks.append(track)
+        }
+        set?.close()
+        return tracks
+    }
+    
     
     //    func deleteTrackPlace(id: Int32) -> Bool{
     //        shareInstance.database?.open()
